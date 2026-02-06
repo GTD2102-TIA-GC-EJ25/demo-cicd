05852543bae11ddac1beae127012f3c1158e9532
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2026-02-06 04:05:50 +0000

    Primer commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function preload() {
+  
+}
+
+function setup() {
+ // put setup code here
+  createCanvas(800, 600)
+}
+
+function draw() {
+  // put drawing code here
+  background(255)
+  square(100,100,100)
+}
